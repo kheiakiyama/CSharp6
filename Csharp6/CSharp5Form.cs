8c46a7286f2813eb1e41e048b86b3deb2b79af98
kheiakiyama <kheiakiyama@gmail.com>
2015-02-21 08:55:51 +0900

    initial commit

---
+++ Csharp6/CSharp5Form.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp6
+{
+    public partial class CSharp5Form : Form
+    {
+        public CSharp5Form()
+        {
+            InitializeComponent();
+        }
+
+        private static string SLEEP_URL = "http://kheiakiyama-sample.azurewebsites.net/sleep.php?SECOND=5";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Text = "Doing...";
+            WebClient client = new WebClient();
+            client.DownloadString(SLEEP_URL);
+            Text = "DONE!!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Text = "Doing...";
+            WebClient client = new WebClient();
+            client.DownloadStringAsync(new Uri(SLEEP_URL));
+            Text = "DONE!!";
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            Text = "Doing...";
+            WebClient client = new WebClient();
+            await client.DownloadStringTaskAsync(SLEEP_URL);
+            Text = "DONE!!";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Text = "Reset";
+        }
+    }
+}
---
+++ Csharp6/CSharp6Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp6
+{
+    public class CSharp6Model
+    {
+        public string Name { get; set; } = "DefaultName";
+        public string GetOnlyName { get; } = "DefaultName";
+
+        public void Dump() => Console.WriteLine(Name);
+
+        public int NameLength() {
+            return Name?.Length ?? 0;
+        }
+    }
+}
